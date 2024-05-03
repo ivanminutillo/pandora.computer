9fc625a94443cfcd8464e4ff4e2f2a60eaba7203
ivan <ivanminutillo@outlook.it>
2024-05-03 17:25:38 +0200

    up

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,32 +6,28 @@ function App() {
   return (
     <main className="max-w-screen-md mx-auto my-12">
       <div className="prose prose-md">
-        <h2>Toward Multi Agents co-owning Communities</h2>
-        <h4>An ongoing experiment in designing new User Experiences for a multi agents world.</h4>
-        <p>The messaging app is a crucial organization tool for most of the today companies, critical for fluent and direct communication among all the team members.
-        It allows startups, collectives, communities of any size, to coordinate remotely and flourish in a p2p way. We know them, most of us live in these systems daily. </p>
-        <p>There is one hack tough. They have been designed for human to human communication.</p>
+        <h2>About Pandora</h2>
+      <p>Imagine if the majority of your team were composed of autonomous agents. <br/>
+    Autonomous agents are software entities that leverage specific skills and behaviors to autonomously understand and interact in their environment. Moreover, they can coordinate with other humans or agents to pursue specific objectives.</p>
 
-        <p>Imagine if the majority of your team is made of autonomous agents. <br/>
-        Autonomous Agents are software that leverages upon a specific set of skills and behaviours to autonomously understand and interact in the environment they live in, plus they can coordinate with other humans or agents to pursue specific objectives.</p>
+    <p>You will need to ask them questions, and they will likely have questions for you as well (they have agency, right?). You will assign tasks to them, and they will autonomously report updates or any issues they encounter. They will coordinate with other agents and make agreements.
+    You will need to share and discuss strategies, visions, updated knowledge, any changes in plans or task assignments.
+    You will want to request reports and check on progress. <br/>
+    Each agent will likely specialize in different areas and possess different skills and knowledge domains, much like humans—but better.</p>
+    <p>Consequently, you will want to grant different roles and permissions to different agents to promote autonomy beyond a micromanagement approach, which will become unsustainable when 90% of the team members surpass you in terms of domain knowledge and skills.</p>
 
-        <p>You need to ask them questions, and they will likely ask questions to you as well (they have agency right?) you will assign them tasks, they will autonomously report updates or any issues they will face, they will coordinate with other agents and make agreements. 
-        You need to share and discuss strategies, visions, updated knowledge, any plan variation or tasks assignment.
-        You will want to ask for reports and check progresses. <br/>
-        Each Agent will likely be specialized in different areas and have different skills and knowledge domains, exactly like humans <b>- but better</b>. </p>
-        <p>Therefore, You will want to grant different roles and permissions to different agents to promote autonomy beyond a micromanagement approach that will be impossible to sustain when the 90% of the team members surpass you in terms of domain knowledge and skills. </p>
+    <p>Agents will need to use external tools to perform a variety of tasks, and some of them will need access to funds to achieve their goals.</p>
 
-        <p> Agents will need to use external tools to perform a variety of tasks and some of them will need access to funds to achieve their goals. </p>
+    <p>At first glance, this may not seem radically different from standard human-to-human interactions, right?</p>
 
-        <p>At first glance, this may not sound radically different from standard human-to-human interactions, isnt it?</p>
+    <p>However, there are a few substantial differences: agents can work 24/7 non-stop and will be able to spawn new agents autonomously (if they have the right permissions). This will soon make humans a minority in most organizations (let's be conservative and assume one human for every ten agents).</p>
 
-        <p>Well there are few substantial differences: agents will work 24h non stop and they will be able to spawn new agents autonomously (if they have the right permission). This will make soon humans a minority in most of the organizations (let be conservative and assumes 1 human each 10 agents).</p>
+    <p>In this scenario, would a chat app be sufficient to manage such emergent complexity?</p>
+    <p>How will you carry out your day-to-day work with an army of agents autonomously coordinating their activities at a speed you cannot match?</p>
+    <p>In this scenario, where the web is the native habitat of agents that will greatly outnumber and outperform humans in knowledge and skills, how will we relate to them and to the web as a whole?</p>
 
-        <p>In this scenario, would a chat app be sufficient to deal with such emergent complexity?</p>
-        <p>How will you carry your day to day work with an army of agents autonomously coordinating their activities at a speed you will not be able to carry with?</p>
-        <p>In this scenario, where the web will be the native habitat of agents that will heavily surpass humans in numbers, knowledge and skills, how will we relate with them and with the web as a whole?</p>
+    <p>This website explores novel ways of leveraging the emergent possibilities of living and thriving in a post AGI world.</p>
 
-        <p>This website explores novel ways of leveraging the emergent possibilities of living and thriving in a world with autonomous agents.</p>
       </div>
     </main>
   )
