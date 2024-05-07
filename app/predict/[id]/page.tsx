61fb1f60eedc8d30f37ba2909e5236e4fe504d82
ivan <ivanminutillo@outlook.it>
2024-05-07 15:40:08 +0200

    up

--- app/predict/[id]/page.tsx
+++ app/predict/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect } from 'react'
 import { useParams } from 'next/navigation'
-import { Check, CircleOff } from "lucide-react"
+import { Check, CircleOff, PartyPopper, Frown } from "lucide-react"
 import { Badge } from "@/components/ui/badge"
 import { weiToXdai } from '@/lib/utils'
 import { Button, buttonVariants } from "@/components/ui/button"
@@ -11,6 +11,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import {LoadingSpinner} from '@/components/ui/spinner'
 import { getBuiltGraphSDK } from '@/.graphclient'
 import { Separator } from '@/components/ui/separator'
 import { useInfiniteQuery } from '@tanstack/react-query';
@@ -143,11 +144,32 @@ function App() {
                   <Card key={i}>
                     <CardHeader>
                       <CardTitle className='font-medium leading-6'>
-                        {trade.title}
+                        <div>
+                        {market_status(trade) == "Closed" ? (
+                          <div className='mb-1.5'>
+                            {trade.fpmm.outcomes ?  trade.fpmm.outcomes[parseInt(trade.fpmm.currentAnswer!, 16)] === trade.fpmm.outcomes[trade.outcomeIndex] ? (
+                              <div className="text-green-500  flex items-end text-sm gap-2">
+                                <PartyPopper className="w-5 h-5" />
+                                <span className='font-normal'>
+                                  {weiToXdai(trade.outcomeTokensTraded)} earned
+                                </span>
+                              </div>
+                            ) : 
+                            (
+                              <div className="text-red-500 flex items-end text-sm gap-2">
+                              <Frown className="w-5 h-5" />
+                              <span className='font-normal'>
+                                Bet lost
+                              </span>
+                            </div>) : ''}
+                          </div>
+                        ) : ""}
+                        <div>{trade.title}</div>
                         <div className='!text-sm mt-1 !font-normal text-muted-foreground flex items-center gap-2'>
                           <span className='text-sm !font-normal text-muted-foreground'>{new Date(trade.creationTimestamp * 1000).toLocaleDateString('en-US', { month: 'short', day: 'numeric' })}</span>
                           <span>·</span>
                           <span>Bought {weiToXdai(trade.collateralAmount)} for {weiToXdai(trade.outcomeTokensTraded)} {trade.fpmm.outcomes ? trade.fpmm.outcomes[trade.outcomeIndex] : 'unknown'} tokens</span>
+                        </div>
                         </div>
                       </CardTitle>
                     </CardHeader>
@@ -159,11 +181,11 @@ function App() {
                             {trade.fpmm.outcomes && trade.fpmm.outcomes[trade.outcomeIndex] == 'Yes' ? 
                             <div className='flex items-center gap-2'>
                               <Check className="w-4 h-4 text-green-500" />
-                              <span className='font-bold text-green-500'>Yes</span>
+                              <span className=''>Yes</span>
                             </div> : 
                             <div className='flex items-center gap-2'>
                               <CircleOff className="w-4 h-4 text-red-500" />
-                              <span className='font-bold text-red-500'>No</span>
+                              <span className=''>No</span>
                             </div>
                             }
                             {/* <span className='font-bold'>{trade.fpmm.outcomes ? trade.fpmm.outcomes[trade.outcomeIndex] : 'unknown'}</span> */}
@@ -173,15 +195,15 @@ function App() {
                             Agent prevision
                           </span>
                         </div>
-                        {/* if market_status(trade) == "Closed" show <span>closed</span> otherwise show <span>{market_status(trade)}</span> */}
                         {market_status(trade) == "Closed" ? (
                           <div className='flex flex-col gap-1'>
                           <div>
-                          {trade.fpmm.outcomes && (
-                              <Badge className='inline'>
-                                {trade.fpmm.outcomes[trade.outcomeIndex]}
+                            {trade.fpmm.outcomes && 
+                              <Badge variant={'outline'}>
+                                 {trade.fpmm.outcomes[parseInt(trade.fpmm.currentAnswer!, 16)]}
                               </Badge>
-                            )}
+                            }
+                            
                           </div>
                           <span className='text-xs uppercase tracking-wider text-muted-foreground'>
                             Final result
@@ -189,9 +211,29 @@ function App() {
                         </div>
                         ) : (
                           <div className='flex flex-col gap-1'>
-                            <div>
-                              <Badge className='inline' variant="outline">{market_status(trade)}</Badge>
-                            </div>
+                            {market_status(trade) === "Open" ? (
+                              <div>
+                                <Badge className='inline-flex items-center gap-2' variant="outline">
+                                  <span className="flex h-2 w-2 relative">
+                                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-purple-400 opacity-75"></span>
+                                    <span className="relative inline-flex rounded-full h-2 w-2 bg-purple-500"></span>
+                                  </span>
+                                  {market_status(trade)}
+                                </Badge>
+                              </div>
+                            ) : market_status(trade) === "Pending" ? (
+                              <div>
+                                <Badge className='inline-flex items-center gap-2' variant="outline">
+                                  <LoadingSpinner className='w-3 h-3' />
+                                  {market_status(trade)}
+                                </Badge>
+                              </div>
+                            ) :  <div>
+                            <Badge className='inline' variant="outline">{market_status(trade)}</Badge>
+                          </div>
+                            }
+                            
+                           
                             <span className='text-xs uppercase tracking-wider text-muted-foreground'>
                               Market status
                             </span>
